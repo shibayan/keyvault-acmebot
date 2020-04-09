a24e2315febda4e72951f0621c2b0a3147ea9a18
Tatsuro Shibamura <me@shibayan.jp>
2020-04-10 01:54:58 +0900

    Adding Web UI page

---
+++ KeyVault.Acmebot/StaticPageFunctions.cs
@@ -0,0 +1,26 @@
+using Azure.WebJobs.Extensions.HttpApi;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace KeyVault.Acmebot
+{
+    public class StaticPageFunctions : HttpFunctionBase
+    {
+        public StaticPageFunctions(IHttpContextAccessor httpContextAccessor)
+            : base(httpContextAccessor)
+        {
+        }
+
+        [FunctionName(nameof(StaticPage))]
+        public IActionResult StaticPage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "static-page/index")] HttpRequest req,
+            ILogger log)
+        {
+            return File("index.html", "text/html");
+        }
+    }
+}
