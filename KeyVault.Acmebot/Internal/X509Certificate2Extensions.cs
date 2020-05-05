cfd3abc0d69186b9acf347393804839a9af9b266
Tatsuro Shibamura <me@shibayan.jp>
2020-05-05 17:26:15 +0900

    Fixed a parse bug in PEM with multiple certificates

--- KeyVault.Acmebot/Internal/X509Certificate2Extensions.cs
+++ KeyVault.Acmebot/Internal/X509Certificate2Extensions.cs
@@ -5,16 +5,30 @@ namespace KeyVault.Acmebot.Internal
 {
     internal static class X509Certificate2Extensions
     {
-        private static ReadOnlySpan<byte> Separator => new byte[] { 0x0A, 0x0A };
+        private static ReadOnlySpan<byte> EndCertificate => new byte[]
+        {
+            0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x45, 0x4e, 0x44, 0x20, 0x43,
+            0x45, 0x52, 0x54, 0x49, 0x46, 0x49, 0x43, 0x41, 0x54, 0x45,
+            0x2d, 0x2d, 0x2d, 0x2d, 0x2d
+        };
 
         public static void ImportFromPem(this X509Certificate2Collection collection, byte[] rawData)
         {
             var rawDataSpan = rawData.AsSpan();
 
-            var separator = rawDataSpan.IndexOf(Separator);
+            while (true)
+            {
+                var foundIndex = rawDataSpan.IndexOf(EndCertificate);
 
-            collection.Add(new X509Certificate2(rawDataSpan.Slice(0, separator).ToArray()));
-            collection.Add(new X509Certificate2(rawDataSpan.Slice(separator + 2).ToArray()));
+                if (foundIndex == -1)
+                {
+                    break;
+                }
+
+                collection.Add(new X509Certificate2(rawDataSpan.Slice(0, foundIndex + EndCertificate.Length).ToArray()));
+
+                rawDataSpan = rawDataSpan.Slice(foundIndex + EndCertificate.Length);
+            }
         }
     }
 }
