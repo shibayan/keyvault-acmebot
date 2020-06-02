a6be582a40a6f78f8e3eca08b15527c1d1db547a
Tatsuro Shibamura <me@shibayan.jp>
2020-06-02 16:02:34 +0900

    Fixed using ordering

--- KeyVault.Acmebot/StaticPageFunctions.cs
+++ KeyVault.Acmebot/StaticPageFunctions.cs
@@ -1,9 +1,9 @@
 using Azure.WebJobs.Extensions.HttpApi;
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace KeyVault.Acmebot
