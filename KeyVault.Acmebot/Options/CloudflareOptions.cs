6eb2c3c673001d87d7a428a026db97944b5bc93e
Tatsuro Shibamura <me@shibayan.jp>
2020-07-21 21:53:48 +0900

    Implemented Cloudflare DNS Provider

--- KeyVault.Acmebot/Options/CloudflareOptions.cs
+++ KeyVault.Acmebot/Options/CloudflareOptions.cs
@@ -2,5 +2,6 @@
 {
     public class CloudflareOptions
     {
+        public string ApiKey { get; set; }
     }
 }
--- KeyVault.Acmebot/Providers/AzureDnsProvider.cs
+++ KeyVault.Acmebot/Providers/AzureDnsProvider.cs
@@ -24,25 +24,25 @@ namespace KeyVault.Acmebot.Providers
 
         private readonly DnsManagementClient _dnsManagementClient;
 
-        public async Task<IList<DnsZone>> ListZonesAsync()
+        public async Task<IReadOnlyList<DnsZone>> ListZonesAsync()
         {
             var zones = await _dnsManagementClient.Zones.ListAllAsync();
 
             return zones.Select(x => new DnsZone { Id = x.Id, Name = x.Name }).ToArray();
         }
 
-        public async Task UpsertTxtRecordAsync(DnsZone zone, string recordName, IEnumerable<string> values)
+        public async Task UpsertTxtRecordAsync(DnsZone zone, string relativeRecordName, IEnumerable<string> values)
         {
             var resourceGroup = ExtractResourceGroup(zone.Id);
 
             // 既存の TXT レコードがあれば取得する
-            var recordSet = await _dnsManagementClient.RecordSets.GetOrDefaultAsync(resourceGroup, zone.Name, recordName, RecordType.TXT) ?? new RecordSet();
+            var recordSet = await _dnsManagementClient.RecordSets.GetOrDefaultAsync(resourceGroup, zone.Name, relativeRecordName, RecordType.TXT) ?? new RecordSet();
 
             // TXT レコードに TTL と値をセットする
             recordSet.TTL = 60;
             recordSet.TxtRecords = values.Select(x => new TxtRecord(new[] { x })).ToArray();
 
-            await _dnsManagementClient.RecordSets.CreateOrUpdateAsync(resourceGroup, zone.Name, recordName, RecordType.TXT, recordSet);
+            await _dnsManagementClient.RecordSets.CreateOrUpdateAsync(resourceGroup, zone.Name, relativeRecordName, RecordType.TXT, recordSet);
         }
 
         private static string ExtractResourceGroup(string resourceId)
