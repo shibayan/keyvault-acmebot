1b6612f99e43a6e8e61892356629892960f8f567
Tatsuro Shibamura <me@shibayan.jp>
2020-09-19 15:17:19 +0900

    Remove unnecessary using

--- KeyVault.Acmebot/Internal/AcmeProtocolClientExtensions.cs
+++ KeyVault.Acmebot/Internal/AcmeProtocolClientExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using System.Threading;
