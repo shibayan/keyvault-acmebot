2aa61b4daddd70453de97802edd6c9f17348318e
Lucas Humenhuk <lucas.decarli@outlook.com>
2021-01-21 12:45:56 -0500

    Add GoDadyOption file

---
+++ KeyVault.Acmebot/Options/GoDaddyOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyVault.Acmebot.Options
+{
+    public class GoDaddyOptions
+    {
+        public string ApiKey { get; set; }
+        public string Secret { get; set; }
+    }
+}
