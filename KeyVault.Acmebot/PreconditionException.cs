c07a97d96fc1f08db2957960dd3fd651ae8a993d
Tatsuro Shibamura <me@shibayan.jp>
2021-01-31 00:13:17 +0900

    Improvement precondition error message (#236)

---
+++ KeyVault.Acmebot/PreconditionException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace KeyVault.Acmebot
+{
+    [Serializable]
+    public class PreconditionException : Exception
+    {
+        public PreconditionException()
+        {
+        }
+
+        public PreconditionException(string message)
+            : base(message)
+        {
+        }
+
+        public PreconditionException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected PreconditionException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
