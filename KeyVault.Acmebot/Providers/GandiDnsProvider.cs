37a1fad64deb7ff7c9a987ec69c3e05c8c20ffd1
Erwin Kramer <erwinkramer@hotmail.com>
2021-10-22 12:19:18 +0200

    zone object

--- KeyVault.Acmebot/Providers/GandiDnsProvider.cs
+++ KeyVault.Acmebot/Providers/GandiDnsProvider.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using KeyVault.Acmebot.Internal;
 using KeyVault.Acmebot.Options;
 
+using Newtonsoft.Json;
+
 namespace KeyVault.Acmebot.Providers
 {
     public class GandiDnsProvider : IDnsProvider
@@ -26,7 +28,7 @@ namespace KeyVault.Acmebot.Providers
         {
             var zones = await _client.ListZonesAsync();
 
-            return zones;
+            return zones.Select(x => new DnsZone { Id = x.Uuid, Name = x.Name, NameServers = [x.PrimaryNameServer] }).ToArray();
         }
 
         public async Task CreateTxtRecordAsync(DnsZone zone, string relativeRecordName, IEnumerable<string> values)
@@ -59,13 +61,13 @@ namespace KeyVault.Acmebot.Providers
 
             private readonly HttpClient _httpClient;
 
-            public async Task<IReadOnlyList<DnsZone>> ListZonesAsync()
+            public async Task<IReadOnlyList<Zone>> ListZonesAsync()
             {
                 var response = await _httpClient.GetAsync("zones");
 
                 response.EnsureSuccessStatusCode();
 
-                return await response.Content.ReadAsAsync<DnsZone[]>();
+                return await response.Content.ReadAsAsync<Zone[]>();
             }
 
             public async Task DeleteRecordAsync(string zoneName, string relativeRecordName)
@@ -88,6 +90,38 @@ namespace KeyVault.Acmebot.Providers
 
                 response.EnsureSuccessStatusCode();
             }
+        }
+        public class Zone
+        {
+            [JsonProperty("uuid")]
+            public string Uuid { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("primary_ns")]
+            public string PrimaryNameServer { get; set; }
+
+            [JsonProperty("email")]
+            public string Email { get; set; }
+
+            [JsonProperty("serial")]
+            public int Serial { get; set; }
+
+            [JsonProperty("user_uuid")]
+            public string UserUuid { get; set; }
+
+            [JsonProperty("refresh")]
+            public int Refresh { get; set; }
+
+            [JsonProperty("minimum")]
+            public int Minimum { get; set; }
+
+            [JsonProperty("expire")]
+            public int Expire { get; set; }
+
+            [JsonProperty("retry")]
+            public int Retry { get; set; }
         }
     }
 }
