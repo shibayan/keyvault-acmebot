97f7a10c63b6509c64b2eda17a57ef5dcc8c9cdd
Tatsuro Shibamura <me@shibayan.jp>
2025-01-06 14:59:27 +0900

    Adding generic webhook extra data (#802)

--- KeyVault.Acmebot/Internal/Constants.cs
+++ KeyVault.Acmebot/Internal/Constants.cs
@@ -1,9 +1,12 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 namespace KeyVault.Acmebot.Internal;
 
 internal static class Constants
 {
+    public static string FunctionAppName { get; } = Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME") ?? "Unknown";
+
     public static string ApplicationVersion { get; } = typeof(Startup).Assembly
                                                                       .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
                                                                       ?.InformationalVersion;
